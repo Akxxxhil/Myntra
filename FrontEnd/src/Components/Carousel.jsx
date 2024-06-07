6fb860b09616b394f1dbaf8ea8181251833952a1
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-06-07 16:58:17 +0530

    updated 2 button

--- FrontEnd/src/Components/Carousel.jsx
+++ FrontEnd/src/Components/Carousel.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Carousel() {
   return (
     <div>
-      <img  src="./images/carousel.jpg" alt="" />
+      <img src="./images/carousel.jpg" alt="" />
     </div>
   )
 }
--- FrontEnd/src/Components/Products.jsx
+++ FrontEnd/src/Components/Products.jsx
@@ -29,45 +29,53 @@ function Product() {
 
   return (
     <div>
-      {loading ? (
-        <Spinner />
-      ) : (
-        <div className="grid grid-cols-3 gap-4 px-3 py-5">
-          {productitem.map((item) => {
-            return (
-              <div key={item.id} className="flex flex-col justify-center items-center gap-3 border-2 border-green-600 rounded-xl">
-                <div>
-                  <img
-                  className="py-4"
-                    width={"100px"}
-                    height={"50px"}
-                    src={item.image}
-                    alt=""
-                  />
-                </div>
-                <div className="text-center">{item.title}</div>
-                <div>₹{item.price}</div>
-                <div className="flex gap-2">
-                  <button
-                    onClick={() => addtobaghandler(item)}
-                    type="button"
-                    className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-                  >
-                    Add To Bag
-                  </button>
-                  <button
-                    onClick={() => wishlistHandler(item)}
-                    type="button"
-                    className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-                  >
-                    Wishlist
-                  </button>
-                </div>
-              </div>
-            );
-          })}
+      <div>
+        <div className="flex justify-center mt-3 gap-3">
+          <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">NEW DROPS</button>
+          <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">MOST TRENDING</button>
         </div>
-      )}
+        <div>
+          {loading ? (
+            <Spinner />
+          ) : (
+            <div className="grid grid-cols-3 gap-4 px-3 py-5">
+              {productitem.map((item) => {
+                return (
+                  <div key={item.id} className="flex flex-col justify-center items-center gap-3 border-2 border-green-600 rounded-xl">
+                    <div>
+                      <img
+                        className="py-4"
+                        width={"100px"}
+                        height={"50px"}
+                        src={item.image}
+                        alt=""
+                      />
+                    </div>
+                    <div className="text-center">{item.title}</div>
+                    <div>₹{item.price}</div>
+                    <div className="flex gap-2">
+                      <button
+                        onClick={() => addtobaghandler(item)}
+                        type="button"
+                        className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                      >
+                        Add To Bag
+                      </button>
+                      <button
+                        onClick={() => wishlistHandler(item)}
+                        type="button"
+                        className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                      >
+                        Wishlist
+                      </button>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
