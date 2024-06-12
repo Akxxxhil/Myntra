ddd1a2f6c44e43e99931339df04d64bce8608e37
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-06-12 14:40:03 +0530

    Wishlist updated

--- FrontEnd/src/Components/Footer.jsx
+++ FrontEnd/src/Components/Footer.jsx
@@ -1,14 +1,15 @@
 function Footer() {
     return (
         <div>
-            <div className="grid grid-cols-4 px-4">
+           <div className="px-3 py-3">
+           <div className="grid grid-cols-4 px-4">
                 <div>
-                    <h4>OFFLINE STORE</h4>
-                    <p>Find Stores Near Me</p>
+                    <h4 className="font-bold ">OFFLINE STORE</h4>
+                    <p className="underline text-sm">Find Stores Near Me</p>
                 </div>
                 <div>
-                    <h4>GET TO KNOW US</h4>
-                    <ul>
+                    <h4 className="font-bold ">GET TO KNOW US</h4>
+                    <ul className="flex flex-col gap-1">
                         <li>Contact Us</li>
                         <li>FAQ's</li>
                         <li>Blogs</li>
@@ -17,24 +18,25 @@ function Footer() {
                 </div>
                 <div>
                     <div>
-                        <h4>TRACK OR</h4>
-                        <h4>RETURN/EXCHANGE ORDER</h4>
-                        <ul>
-                            <li>TRACK ORDER</li>
-                            <li>PLACE RETURN/EXCHANGE REQUEST</li>
-                            <li>RETURNS/EXCHANGE POLICY</li>
+                        <p className="font-bold ">TRACK OR</p>
+                        <p className="font-bold ">RETURN/EXCHANGE ORDER</p>
+                        <ul className="flex flex-col gap-1">
+                            <li>Track Order</li>
+                            <li>Place RETURN/EXCHANGE Request</li>
+                            <li>RETURNS/EXCHANGE policy</li>
                         </ul>
                     </div>
                 </div>
                 <div>
-                    <h4>CUSTOMER CARE</h4>
-                    <ul>
+                    <h4 className="font-bold ">CUSTOMER CARE</h4>
+                    <ul className="flex flex-col gap-1">
                         <li>Timings: 10 AM - 7 PM (Mon - Sat)</li>
                         <li>Whatsapp : +91 6366966283</li>
                         <li>Instagram: @snitch.co.in</li>
                     </ul>
                 </div>
             </div>
+           </div>
         </div>
     )
 }
--- FrontEnd/src/Pages/Wishlist.jsx
+++ FrontEnd/src/Pages/Wishlist.jsx
@@ -1,31 +1,59 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-
+import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 function Wishlist() {
-  const navigate=useNavigate()
-  const productitem = useSelector((state) => state.wishlist)
+  const navigate = useNavigate();
+  const productitem = useSelector((state) => state.wishlist);
   return (
     <div>
-      {productitem.map((item) => {
-        return <div key={item.id}>
-          <div className='flex gap-3 border-4 border-green-600'>
-            <div>
-              <div>{item.title}</div>
-              <div>
-                <img width={"100px"} height={"50px"} src={item.image} alt="" />
+      {productitem.length !== 0 ? (
+        <div className="grid grid-cols-3 gap-4 h-[200px] p-5 w-[900px]">
+          {productitem.map((item) => {
+            return (
+              <div
+                key={item.id}
+                className="flex gap-3 border-4 border-green-600 items-center justify-center h-[200px]"
+              >
+                <div className="flex flex-col items-center gap-3 ">
+                  <div className="mb-2">{item.title}</div>
+                  <div>
+                    <img
+                      width={"50px"}
+                      height={"50px"}
+                      src={item.image}
+                      alt={item.title}
+                    />
+                  </div>
+                  <div>
+                    <button
+                      type="button"
+                      class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                    >
+                      Add to Bag
+                    </button>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div>
-              {/* <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Add to Bag</button> */}
-            </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="flex flex-col justify-center items-center h-screen">
+          <div className="flex flex-col gap-4 justify-center items-center ">
+            Your WishList is Empty BOSS. Please Fill Something
+            <button
+              onClick={() => navigate("/")}
+              type="button"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Home
+            </button>
           </div>
         </div>
-      })}
-      <button onClick={()=>navigate("/")} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Home</button>
+      )}
     </div>
-  )
+  );
 }
 
-export default Wishlist
+export default Wishlist;
